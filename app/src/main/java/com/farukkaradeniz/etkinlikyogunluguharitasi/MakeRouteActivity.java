67388274a485416cc185527caaa40d23e722b565
Faruk Karadeniz <krdnzomer@gmail.com>
2018-05-20 23:33:13 +0300

    Routes can be created and shown on the map

---
+++ app/src/main/java/com/farukkaradeniz/etkinlikyogunluguharitasi/MakeRouteActivity.java
@@ -0,0 +1,120 @@
+package com.farukkaradeniz.etkinlikyogunluguharitasi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.directions.route.AbstractRouting;
+import com.directions.route.Route;
+import com.directions.route.RouteException;
+import com.directions.route.Routing;
+import com.directions.route.RoutingListener;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+public class MakeRouteActivity extends AppCompatActivity implements RoutingListener {
+    private static final String TAG = MakeRouteActivity.class.getSimpleName();
+    private Place source, destination;
+    private PlaceAutocompleteFragment sourcePlaceFragment, destinationPlaceFragment;
+    private Button makeRoute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_route);
+        init();
+        listeners();
+    }
+
+    private void init() {
+        makeRoute = findViewById(R.id.btn_make_route);
+        sourcePlaceFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.start_destination);
+
+        destinationPlaceFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.stop_destination);
+
+        sourcePlaceFragment.setHint("Başlangıç konumu giriniz");
+        destinationPlaceFragment.setHint("Bitiş konumunu giriniz");
+    }
+
+
+    private void listeners() {
+        sourcePlaceFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Log.i(TAG, "Place: " + place.toString());
+                source = place;
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.i(TAG, "An error occurred: " + status);
+                Toast.makeText(MakeRouteActivity.this, "Error_S: " + status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        destinationPlaceFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Log.i(TAG, "Place2: " + place.toString());
+                destination = place;
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.i(TAG, "An error occurred2: " + status);
+                Toast.makeText(MakeRouteActivity.this, "Error_D: " + status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        makeRoute.setOnClickListener(view -> createRoute());
+    }
+
+    private void createRoute() {
+        if (source == null || destination == null) {
+            Toast.makeText(this, "Unvalid locations", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Routing routing = new Routing.Builder()
+                .alternativeRoutes(false)
+                .travelMode(AbstractRouting.TravelMode.DRIVING)
+                .waypoints(source.getLatLng(), destination.getLatLng())
+                .withListener(this)
+                .build();
+        routing.execute();
+
+    }
+
+    @Override
+    public void onRoutingFailure(RouteException e) {
+
+    }
+
+    @Override
+    public void onRoutingStart() {
+
+    }
+
+    @Override
+    public void onRoutingSuccess(ArrayList<Route> arrayList, int i) {
+        Route route = arrayList.get(0);
+        Intent intent = getIntent();
+        ArrayList<LatLng> points = ((ArrayList<LatLng>) route.getPoints());
+        intent.putParcelableArrayListExtra("points", points);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    public void onRoutingCancelled() {
+
+    }
+}
--- app/src/main/java/com/farukkaradeniz/etkinlikyogunluguharitasi/MapsActivity.java
+++ app/src/main/java/com/farukkaradeniz/etkinlikyogunluguharitasi/MapsActivity.java
@@ -1,12 +1,16 @@
 package com.farukkaradeniz.etkinlikyogunluguharitasi;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,29 +18,50 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final String TAG = MapsActivity.class.getSimpleName();
     private GoogleMap mMap;
     private UiSettings uiSettings;
-    private FloatingActionButton showEvent, makeRoute;
+    private FloatingActionButton showEvent, makeRoute, clearMap;
+    private FloatingActionsMenu floatMenu;
+    private Polyline route;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         init();
-
+        listeners();
 
     }
 
     private void init() {
         showEvent = findViewById(R.id.action_event);
         makeRoute = findViewById(R.id.action_route);
+        clearMap = findViewById(R.id.action_clear);
+        floatMenu = findViewById(R.id.float_menu);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+    private void listeners() {
+        makeRoute.setOnClickListener(view -> {
+            startActivityForResult(new Intent(this, MakeRouteActivity.class), 11);
+        });
+        showEvent.setOnClickListener(view -> {
+            Toast.makeText(this, "Show Event", Toast.LENGTH_SHORT).show();
+        });
+        clearMap.setOnClickListener(view -> {
+            Toast.makeText(this, "Clear the Map", Toast.LENGTH_SHORT).show();
+        });
     }
 
 
@@ -49,7 +74,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         uiSettings.setCompassEnabled(true);
         uiSettings.setMyLocationButtonEnabled(true);
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(41, 29)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(41.052, 29.001), 14f));
         mMap.addMarker(new MarkerOptions().position(new LatLng(41, 29)).title("XXXXX"));
     }
 
@@ -65,5 +90,27 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             Toast.makeText(this, "Traffic", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.i(TAG, "onActivityResult: ");
+        if (requestCode == 11 && resultCode == RESULT_OK) {
+            floatMenu.collapse();
+            ArrayList<LatLng> points = data.getParcelableArrayListExtra("points");
+            createRoute(points);
+            Log.i(TAG, "onActivityResult: Route points size: " + points.size());
+        }
+    }
+
+    private void createRoute(ArrayList<LatLng> points) {
+        if (route != null && route.isVisible()) {
+            route.remove();
+        }
+        PolylineOptions options = new PolylineOptions();
+        options.addAll(points);
+        options.width(10);
+        options.color(Color.BLUE);
+        route = mMap.addPolyline(options);
     }
 }
