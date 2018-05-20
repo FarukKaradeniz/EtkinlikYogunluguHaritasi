34e6e981c6312d3baed418fa15008a780720b9cc
Faruk Karadeniz <krdnzomer@gmail.com>
2018-05-20 17:52:19 +0300

    UI updates

--- app/src/main/java/com/farukkaradeniz/etkinlikyogunluguharitasi/MainActivity.java
+++ app/src/main/java/com/farukkaradeniz/etkinlikyogunluguharitasi/MainActivity.java
@@ -1,13 +1,18 @@
 package com.farukkaradeniz.etkinlikyogunluguharitasi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(v -> {
+            startActivity(new Intent(this, MapsActivity.class));
+        });
     }
 }
--- app/src/main/java/com/farukkaradeniz/etkinlikyogunluguharitasi/MapsActivity.java
+++ app/src/main/java/com/farukkaradeniz/etkinlikyogunluguharitasi/MapsActivity.java
@@ -1,22 +1,38 @@
 package com.farukkaradeniz.etkinlikyogunluguharitasi;
 
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private UiSettings uiSettings;
+    private FloatingActionButton showEvent, makeRoute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        init();
+
+
+    }
+
+    private void init() {
+        showEvent = findViewById(R.id.action_event);
+        makeRoute = findViewById(R.id.action_route);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -27,7 +43,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        uiSettings = mMap.getUiSettings();
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setMapToolbarEnabled(true);
+        uiSettings.setCompassEnabled(true);
+        uiSettings.setMyLocationButtonEnabled(true);
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(41, 29)));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(41, 29)).title("XXXXX"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.maps_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.traffic) {
+            Toast.makeText(this, "Traffic", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
